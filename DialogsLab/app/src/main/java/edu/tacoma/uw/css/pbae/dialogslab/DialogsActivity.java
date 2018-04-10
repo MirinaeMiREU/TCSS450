8dc91bf0bb834a8e3e29bb5eafeff19437bc5a91
Peter Bae <pbae@uw.edu>
2018-04-09 21:09:59 -0700

    Dialogs lab complete

---
+++ DialogsLab/app/src/main/java/edu/tacoma/uw/css/pbae/dialogslab/DialogsActivity.java
@@ -0,0 +1,47 @@
+package edu.tacoma.uw.css.pbae.dialogslab;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class DialogsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogs);
+    }
+
+    public void launch(View view) {
+        DialogFragment fragment = null;
+        if (view.getId() == R.id.btn_fire_missiles) {
+            fragment = new FireMissilesDialogFragment();
+        } else if (view.getId() == R.id.btn_launch_colors) {
+            fragment = new ListDialogFragment();
+        } else if (view.getId() == R.id.btn_launch_toppings) {
+            fragment = new MultiListDialogFragment();
+        } else if (view.getId() == R.id.btn_custom_signin) {
+            fragment = new CustomDialogFragment();
+        } else if (view.getId() == R.id.btn_notification) {
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this,
+                    "default")
+                    .setSmallIcon(R.drawable.icon)
+                    .setContentTitle("Hello")
+                    .setContentText("How are ya?")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(1991, mBuilder.build());
+            new NotificationFragment();
+        }
+
+
+        if (fragment != null)
+            fragment.show(getSupportFragmentManager(), "launch");
+    }
+
+}
